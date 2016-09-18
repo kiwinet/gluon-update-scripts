1756cef421a0f89f4280a9737df938cf1b50c334
kiwinet <avita@hostega.eu>
2016-09-18 16:28:11 +0300

    .

--- init_build.sh
+++ init_build.sh
@@ -7,9 +7,6 @@ if [ -z "$1" ]
  then
 	print 'stable, beta or experimental 1'
 	exit 1
-elif [[ "$1" != "stable" || "$1" != "beta" || "$1" != "experimental" ]]; then
-	print 'stable, beta or experimental 2'
-	exit 1
 fi
 
 
