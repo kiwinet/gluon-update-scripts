ed08d0742ee38b8f27362b0d9a7fa7847c0bed6e
kiwinet <avita@hostega.eu>
2016-09-18 15:31:13 +0300

    .

--- config.sh
+++ config.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-BASE_DIR = '/otp/gluon-update'
-BRANCH = 'stable'
-GLUON_RELISE = 'v2016.1.6'
-REPO = 'https://github.com/freifunk-gluon/gluon.git'
+BASE_DIR = "/otp/gluon-update"
+BRANCH = "stable"
+GLUON_RELISE = "v2016.1.6"
+REPO = "https://github.com/freifunk-gluon/gluon.git"
 
 #site repo
-SITE_REPO = 'https://github.com/kiwinet/site-ksia'
+SITE_REPO = "https://github.com/kiwinet/site-ksia"
 
