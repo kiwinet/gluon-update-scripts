d20c548500724bfa62afc9b4c1039a1d836ddbd7
kiwinet <avita@hostega.eu>
2016-09-18 15:46:17 +0300

    .

--- init_build.sh
+++ init_build.sh
@@ -3,16 +3,16 @@
 
 source /opt/gluon-update-scripts/config.sh
 
-if [ -z $1 ] or [ $1 != 'stable' ] or [ $1 != 'beta' ] or [ $1 != 'experimental' ]; then
+if [ -z "$1" ] or [ "$1" != 'stable' ] or [ "$1" != 'beta' ] or [ "$1" != 'experimental' ]; then
 	print 'stable, beta or experimental'
 	exit 1
 fi
 
-if [ ! -d $BASE_DIR ]; then
+if [ ! -d "$BASE_DIR" ]; then
 	mkdir $BASE_DIR
 fi
 
-if [ -d $BASE_DIR/$1 ]; then
+if [ -d "$BASE_DIR/$1" ]; then
 	cd $BASE_DIR/$1
 else
 	mkdir $BASE_DIR/$1
