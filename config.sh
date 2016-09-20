7d566dcd804965264fa83e01ddb37bf582192397
kiwinet <avita@hostega.eu>
2016-09-20 21:01:37 +0300

    .

--- config.sh
+++ config.sh
@@ -14,7 +14,7 @@ USER="u1227"
 TARGETS="ar71xx-generic ar71xx-nand"
 #TARGETS="ar71xx-generic ar71xx-nand mpc85xx-generic x86-xen_domu x86-kvm_guest"
 TARGETSx86="x86-generic x86-64"
-THREADS="8"
+THREADS="4"
 
 ##
 ##
