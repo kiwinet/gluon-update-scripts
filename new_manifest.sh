d0c7e43529cf35bb6a72b9f4261d4476e3aa6a45
kiwinet <avita@hostega.eu>
2016-09-20 23:30:50 +0300

    .

---
+++ new_manifest.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+
+##
+## Config
+##
+MAIN_DIR="/opt/gluon-update-scripts"
+
+##
+## Body
+##
+NEW="0"
+source $MAIN_DIR/config.sh
+
+BRANCH=$BRANCH_S
+
+T="$(date +"%Y-%m-%d.%H%M")"
+RELEASE_TAG="$GLUON_RELEASE.${BRANCH:0:1}.$T"
+MY_RELEASE="${GLUON_RELEASE:1}-$BRANCH-$T"
+
+{
+
+if [ -d "$BASE_DIR/$BRANCH/gluon/output/images" ]; then
+	if [ -d "$BASE_DIR/$BRANCH/gluon/output/images/sysupgrade" ]; then
+		cd $BASE_DIR/$BRANCH/gluon/output/images/sysupgrade
+		rm -f md5sums
+		rm -f *.manifest
+		md5sum * >> md5sums
+	fi
+
+	if [ -d "$BASE_DIR/$BRANCH/gluon/output/images/factory" ]; then
+		cd $BASE_DIR/$BRANCH/gluon/output/images/factory
+		rm -f md5sums
+		rm -f *.manifest
+		md5sum * >> md5sums
+	fi
+
+	echo "> make manifest"
+	date
+	cd $BASE_DIR/$BRANCH/gluon
+
+	#/usr/bin/sudo -u $USER 
+	make manifest GLUON_BRANCH=$BRANCH GLUON_RELEASE=$MY_RELEASE
+exit 1
+	cd $MAIN_DIR
+
+	#/usr/bin/sudo -u $USER 
+	$MAIN_DIR/sign.sh $MAIN_DIR/secret $BASE_DIR/$BRANCH/gluon/output/images/sysupgrade/$BRANCH.manifest
+
+	cd $BASE_DIR/$BRANCH/gluon
+
+	if [ -a "$BASE_DIR/$BRANCH/gluon/output/images/sysupgrade/$BRANCH.manifest" ]; then
+
+		/bin/mkdir -p $HTML_IMAGES_DIR/archive/$BRANCH/
+
+		if [ -a "$HTML_IMAGES_DIR/$BRANCH/archive/images/sysupgrade/md5sums" ]; then
+			/bin/more $HTML_IMAGES_DIR/$BRANCH/images/sysupgrade/md5sums >> $HTML_IMAGES_DIR/$BRANCH/archive/images/sysupgrade/md5sums
+			/bin/rm -f $HTML_IMAGES_DIR/$BRANCH/images/sysupgrade/md5sums
+		fi
+		if [ -a "$HTML_IMAGES_DIR/$BRANCH/archive/images/factory/md5sums" ]; then
+			/bin/more $HTML_IMAGES_DIR/$BRANCH/images/factory/md5sums >> $HTML_IMAGES_DIR/$BRANCH/archive/images/factory/md5sums
+			/bin/rm -f $HTML_IMAGES_DIR/$BRANCH/images/factory/md5sums
+		fi
+
+		/bin/rm -f $HTML_IMAGES_DIR/$BRANCH/images/sysupgrade/$BRANCH.manifest
+		/bin/mv -fb -t $HTML_IMAGES_DIR/archive/$BRANCH $HTML_IMAGES_DIR/$BRANCH/images
+		/bin/mv -fb -t $HTML_IMAGES_DIR/archive/$BRANCH $HTML_IMAGES_DIR/$BRANCH/modules
+
+		/bin/mkdir -p $HTML_IMAGES_DIR/$BRANCH
+		/bin/cp -rf -t $HTML_IMAGES_DIR/$BRANCH $BASE_DIR/$BRANCH/gluon/output/images
+		/bin/cp -rf -t $HTML_IMAGES_DIR/$BRANCH $BASE_DIR/$BRANCH/gluon/output/modules
+		/bin/cp -fb $HTML_IMAGES_DIR/$BRANCH/images/sysupgrade/$BRANCH.manifest $HTML_IMAGES_DIR/$BRANCH/images/sysupgrade/$BRANCH-$T.manifest
+
+		/bin/chown -R $USER:$USER $HTML_IMAGES_DIR
+	fi
+
+fi
+} > >(tee -a /var/log/firmware-build/$MY_RELEASE-manifest.log) 2> >(tee -a /var/log/firmware-build/$MY_RELEASE-manifest.error.log | tee -a /var/log/firmware-build/$MY_RELEASE-manifest.log >&2)
