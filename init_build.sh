9c7cbc368f6b4bda00ff79a2f6973dfd76db770a
kiwinet <avita@hostega.eu>
2017-04-22 01:24:19 +0300

    fix

--- init_build.sh
+++ init_build.sh
@@ -8,6 +8,37 @@ if [ -z "$1" ]; then
 	exit 1
 fi
 
+if [ "$1" == "stable" ]; then
+	BRANCH=$BRANCH_S
+	GLUON_SITE_RELEASE=$SITE_RELEASE_S
+	NEW_RELEASE=$NEW_RELEASE_S
+	TARGETS=$TARGETS_S
+	GLUON_RELEASE=$GLUON_RELEASE_S
+	BROKEN=$BROKEN_S
+elif [ "$1" == "beta" ]; then
+	BRANCH=$BRANCH_B
+	GLUON_SITE_RELEASE=$SITE_RELEASE_B
+	NEW_RELEASE=$NEW_RELEASE_B
+	TARGETS=$TARGETS_B
+	GLUON_RELEASE=$GLUON_RELEASE_B
+	BROKEN=$BROKEN_B
+	echo 'BETA not exist'
+	exit 1
+elif [ "$1" == "experimental" ]; then
+	BRANCH=$BRANCH_E
+	GLUON_SITE_RELEASE=$SITE_RELEASE_E
+	NEW_RELEASE=$NEW_RELEASE_E
+	TARGETS=$TARGETS_E
+	GLUON_RELEASE=$GLUON_RELEASE_E
+	BROKEN=$BROKEN_E
+else
+	echo 'stable, beta or experimental 1'
+	exit 1
+fi
+
+RELEASE_TAG="$NEW_RELEASE.${BRANCH:0:1}.$T"
+MY_RELEASE="${NEW_RELEASE:1}-$BRANCH-$T"
+
 if [ ! -d "$BASE_DIR" ]; then
 	/bin/mkdir -p $BASE_DIR
 fi
